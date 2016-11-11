77f0059ca7cc90403026514eae857ec9fa9a62e1
Kyrre Laugerud Moe <kyrrelm@ntnu.no>
2016-11-11 15:05:13 +0100

    email and password prompt

--- hal.js
+++ hal.js
@@ -1,27 +1,29 @@
 var login = require("facebook-chat-api");
 var prompt = require('prompt');
 
-// 
-// Start the prompt 
-// 
+var schema = {
+properties: {
+  email: {
+    message: 'email',
+    required: true
+  },
+  password: {
+  	message: 'password',
+  	required: true,
+    hidden: true
+  }
+}
+};
+
 prompt.start();
 
-// 
-// Get two properties from the user: username and email 
-// 
-prompt.get(['username', 'email'], function (err, result) {
-// 
-// Log the results. 
-// 
-console.log('Command-line input received:');
-console.log('  username: ' + result.username);
-console.log('  email: ' + result.email);
+prompt.get(schema, function (err, result) {
+	loginAndListen(result.email, result.password);
 });
 
-function loginAndListen(){
-	login({email: "", password: ""}, function callback (err, api) {
+function loginAndListen(emailInput,passwordInput){
+	login({email: emailInput, password: passwordInput}, function callback (err, api) {
 	    if(err) return console.error(err);
-	    console.log("logged in");
 	    api.listen(function callback(err, message) {
 	    	var reply = "";
 	    	if (shouldShowWelcomeMessage(message)) {
@@ -33,7 +35,6 @@ function loginAndListen(){
 	    		}
 	    	}
 	        api.sendMessage(reply, message.threadID);
-	        //api.sendMessage("Jeg er en bot", message.threadID);
 	    });
 	});
 }
